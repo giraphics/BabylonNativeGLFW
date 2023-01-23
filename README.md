f4840c8920e8ddb7618a255d61eef25b9740b381
Parminder Singh <parminder.singh@motional.com>
2023-01-23 18:21:57 +0800

    Updated read and program.

--- README.md
+++ README.md
@@ -1,57 +1,36 @@
-# Babylon Native GLFW
+# Hello world in Typescript
+Simple typescript program in BabylonNative.
 
-Example on how to create a cross platform application using GLFW and Babylon Native. It creates a simple GLFW window and performs the required setup for Babylon Native to render into that window. The rendering logic can be controlled by the game.js script located under Scripts/game.js.
-
-## Getting started
-
-This repo uses npm to download the babylon.js dependecies, and CMake to create the platform specific project. It is required to run npm install before CMake since it will try to copy the javascript files from the babylon.js packages into the generated solution. 
-
-### Mac
+> **Note:** This project is only tested on Mac OS.
+ 
+## Clone
 ```
+git clone --recurse-submodules -j8 git@github.com:giraphics/BabylonNativeGLFW.git
+```
+  
+## Checkout branch
+Go to **BabylonNativeGLFW** directory
+```
+git checkout typescriptBabylonJS
+```
+
+##  Using NPM version v16.16.0 and install
+```
+npm use v16.16.0
 npm install
+```
+
+##  Building
+Create a build folder and executing build commands.
+
+```
 mkdir build
 cd build
 cmake .. -G "Xcode"
 ```
 
-### Windows
-```
-npm install
-mkdir build
-cd build
-cmake .. -G "Visual Studio 16 2019"
-```
-
-### Linux
-
-#### Install CMake:
-
-Babylon Native requires a cmake version higher than the one installed using default ```apt-get install cmake```. It is required that a higher cmake version is installed manually. For instructions on how do install cmake manually please follow the official documentation:
-
-[Install CMake](https://cmake.org/install/)
-
-#### Install dependecies:
+##  Open Project
 
 ```
-sudo apt-get install libxi-dev libxcursor-dev libxinerama-dev libglfw3-dev libgl1-mesa-dev libcurl4-openssl-dev clang-9 libc++-9-dev libc++abi-9-dev lld-9 ninja-build
-```
-
-#### Install V8:
-
-```
-sudo apt-get install libv8-dev
-```
-
-#### Build:
-```
-npm install
-mkdir build
-cd build
-cmake -G Ninja -D NAPI_JAVASCRIPT_ENGINE=V8
-ninja
-```
-
-#### Run:
-```
-./BabylonNativeExample
+open BabylonNativeExample.xcodeproj
 ```
--- Scripts/game.js
+++ Scripts/game.js
@@ -5,12 +5,12 @@
 
 //-------------------- YOUR CODE GOES HERE ------------------------------
 
-function  CreateScene(engine:BABYLON.NativeEngine) : BABYLON.Scene {
+function  CreateScene(engine : BABYLON.NativeEngine) : BABYLON.Scene {
     // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+    const scene = new BABYLON.Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -19,26 +19,30 @@ function  CreateScene(engine:BABYLON.NativeEngine) : BABYLON.Scene {
     //camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
     // Our built-in 'sphere' shape. Params: name, options, scene
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+    let sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
 
     // Move the sphere upward 1/2 its height
     sphere.position.y = 1;
 
     // Our built-in 'ground' shape. Params: name, options, scene
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
     return scene;
 }
 
-var engine = new BABYLON.NativeEngine();
-var scene = CreateScene(engine);
+function  runEngine() {
+    var engine = new BABYLON.NativeEngine();
+    var scene = CreateScene(engine);
 
-engine.runRenderLoop(function () {
-    scene.render();
-});
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+}
+
+runEngine();
